b96eda6cd812f6b2b7166aeb3ed6867ac9291922
Matt Giguere <matthew.giguere@gmail.com>
2015-02-01 23:08:55 -0500

    changed default value
    to the Table Center Temp

--- php/getNewData.php
+++ php/getNewData.php
@@ -51,7 +51,7 @@
     }
 
         $myquery = "
-    SELECT sampleTime as date, tableTempLow as ydata, tableCenterTemp, gratingTemp, coudeTemp FROM environ WHERE sampleTime > '" . $begDate . "' AND sampleTime < '" . $endDate . "';
+    SELECT sampleTime as date, tableCenterTemp as ydata, tableCenterTemp, gratingTemp, coudeTemp FROM environ WHERE sampleTime > '" . $begDate . "' AND sampleTime < '" . $endDate . "';
     ";   
 
     $query = mysql_query($myquery);
